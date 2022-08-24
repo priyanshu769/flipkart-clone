da0cbc62cca6399ea3cd51632a7642e9176c8927
Priyanshu <prynsu@yahoo.com>
2022-08-24 21:26:51 +0530

    Feat: Added navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { ProductsPage } from './Pages'
 function App() {
   return (
     <div className="App">
+    <nav className='navbar'>
+      <img className='logoImg' alt='flipkart logo' src='https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png'></img>
+      <span>Mock</span>
+    </nav>
       <ProductsPage />
     </div>
   );
